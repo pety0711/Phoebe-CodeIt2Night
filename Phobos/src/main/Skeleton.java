fd7c141caadfee66ed4b687b42eb1abc286adb01
Dávid Sebők <sebidewyd@gmail.com>
2015-03-20 16:25:53 +0100

    Menü finomítása. Arena usecase részben lefut.

--- Phobos/src/main/Skeleton.java
+++ Phobos/src/main/Skeleton.java
@@ -13,75 +13,96 @@ import java.io.InputStreamReader;
  */
 public class Skeleton {
 	
+//	StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+//	for (StackTraceElement s : ste) {
+//		System.out.println(s.getClassName() + " " + s.getMethodName());
+//	}
+//	System.out.println(ste.length);
+	static String[] useCaseOptions = {"New game",
+			"Put putty",
+			"Put oil",
+			"Stepping on a putty",
+			"Stepping on an oil",
+			"Step on a safezone",
+			"Stepping on a dangerzone",
+			"Collision",
+			"Finish game"};
 	
+	static char[] requiredInputs = {'1','2','3','4','5','6','7','8','9'};
 	public static void main(String [ ] args)
 	{
+		boolean runMain = true;
+		do {
+			try {
+				Runtime.getRuntime().exec("cls");
+			} catch (IOException e) {
+				for (int i = 0; i < 50; i++) {
+					System.out.println();
+				}
+			}
+			printOutINFO("Start Use-case Test!");
+		     printOutDEBUG("Debug");
+		     //
+		     String[] emptyStrArray = new String[]{""};
+		     Arena a = new Arena();
+		     
+		     char selectedUsecase = printMenuItems(); //Bekérjük a felhasználótól a választandó Use-case számát
+		     
+		     System.out.println(selectedUsecase);
+		     switch(selectedUsecase){
+			     case '0': 
+			    	 
+			    	 break;
+			     case '1':
+			    	 printOutINFO("Selected Use-case:\t"+useCaseOptions[2]);
+			    	 printLastCalledFunction("a", emptyStrArray);
+			    	 break;
+			     case '2': //doWork
+			    	 break;
+			     case '3': //doWork
+			    	 break;
+			     case '4': //doWork
+			    	 break;
+			     case '5': //doWork
+			    	 break;
+			     case '6': //doWork
+			    	 break;
+			     case '7': //doWork
+			    	 break;
+			     case '8': //doWork
+			    	 break;
+			     case '9': //doWork
+			    	 break;
+				 default: 
+					 break;
+		     }
+		     
+		} while (runMain);
+		
 		//Start 
-		 printOutINFO("Start Use-case Test!");
-	     printOutDEBUG("Debug");
-	     //
-	     Arena a = new Arena();
-	     Robot robot = new Robot();
-//	     char r = printMenuItems();
-//	     System.out.print(r);
-	     //New main
-	     
-	     
-	     char selectedUsecase = printMenuItems();
-	     switch(selectedUsecase){
-		     case 0: System.out.println("0 selected");
-		    	 break;
-		     case 1: System.out.println("1 selected");
-		    	 break;
-		     case 2: //doWork
-		    	 break;
-		     case 3: //doWork
-		    	 break;
-		     case 4: //doWork
-		    	 break;
-		     case 5: //doWork
-		    	 break;
-		     case 6: //doWork
-		    	 break;
-		     case 7: //doWork
-		    	 break;
-		     case 8: //doWork
-		    	 break;
-		     case 9: //doWork
-		    	 break;
-			 default: 
-				 break;
-	     }
-	     
+		 
 	}
 	
+	
 	//Kiiratások---------------------------------------------------------------------------------------------------------
-	public static char printMenuItems(){
-		String[] description = {"New game",
-				"Put putty",
-				"Put oil",
-				"Stepping on a putty",
-				"Stepping on an oil",
-				"Step on a safezone",
-				"Stepping on a dangerzone",
-				"Collision",
-				"Finish game",
-				"Close test program"};
-		
-//		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-//		for (StackTraceElement s : ste) {
-//			System.out.println(s.getClassName() + " " + s.getMethodName());
-//		}
-//		System.out.println(ste.length);
 
-		char[] requiredInputs = {'1','2','3','4','5','6','7','8','9','0'};
-		char result = '1';
+	/**
+	 * Prints the menu items.
+	 *
+	 * @return the char
+	 */
+	public static char printMenuItems(){
+		
+		
+		char result = 'o';
+		
 		try {
-			result = requestUserInput(description, requiredInputs);
+			result = requestUserInput(useCaseOptions, requiredInputs);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return result;
 	}
 	
@@ -129,6 +150,11 @@ public class Skeleton {
 		
 	} 
 	
+	/**
+	 * Prints the out info.
+	 *
+	 * @param s the s
+	 */
 	public static void printOutINFO(String s) {
 		System.out.println("INFO>\t" + s);
 	}
@@ -137,6 +163,14 @@ public class Skeleton {
 		System.out.println("DEBUG>\t" + s);
 	}
 	
+	/**
+	 * Request user input.
+	 *
+	 * @param description the description
+	 * @param requiredInputs the required inputs
+	 * @return the char
+	 * @throws Exception the exception
+	 */
 	public static char requestUserInput(String[] description, char[] requiredInputs) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println();
