571c894abfc257f7d38b65d144ce4514c12cf498
Ákos <kos@Jesus>
2015-04-17 12:48:52 +0200

    getRobotId átírása

--- Phobos/src/main/Field.java
+++ Phobos/src/main/Field.java
@@ -4,7 +4,7 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.List; 
+import java.util.List;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -12,59 +12,36 @@ import java.util.List;
  *
  * @author Dávid
  */
-public abstract class  Field {
-	 
-	/** The neighbours. */
+
+public abstract class Field {
+
+	/** The neighbours. 
+	 * 
+	 * Important!
+	 * neighbours[0] is the neighbour at direction [-1; 0]
+	 * neighbours[1] is the neighbour at direction [ 0; 1]
+	 * neighbours[2] is the neighbour at direction [ 1; 0]
+	 * neighbours[3] is the neighbour at direction [ 0;-1]
+	 * 
+	 * */
 	protected List<Field> neighbours;
-	
+
 	/** The robots. */
 	protected List<Robi> robots;
-	
+
 	/** The patches. */
 	protected ArrayList<Patch> patches;
-	
+
 	/** The arena. */
 	protected Arena arena;
-	
+
 	/** The coord. */
 	protected CoordVector coord;
-	
+
 	/** The id. */
 	public String id;
 
 	
-	
-	/**
-	 * Gets the robot id.
-	 *
-	 * @return the robot id
-	 */
-	public String getRobotId() {
-		return robots.get(0).id;
-	}
-	
-	/**
-	 * Step.
-	 *
-	 * @param speed the speed
-	 * @param r the r
-	 * @return the field
-	 */
-	public abstract Field step(CoordVector speed, Robi r);
-	
-	/**
-	 * Stepped on you.
-	 *
-	 * @param r the r
-	 */
-	public abstract void steppedOnYou(Robi r);
-	
-	/**
-	 * Investigate collision.
-	 *
-	 * @throws Exception the exception
-	 */
-	public abstract void investigateCollision() throws Exception;
 	
 	/**
 	 * Instantiates a new field.
@@ -72,65 +49,107 @@ public abstract class  Field {
 	public Field() {
 		patches = new ArrayList<Patch>();
 		robots = new ArrayList<Robi>();
-		// this.id = id; Ez kell bele?
 	}
 	
+	/**
+	 * The method returns with the List of the IDs of Robots on the field
+	 * @return the List of the IDs
+	 */
+	public List<String> getRobotId() {		//R
+		List<String> temp = new ArrayList<String>();
+		for (int i = 0; i < robots.size(); i++) {
+			temp.add(robots.get(i).id);
+		}
+		return temp;
+	}
+
+	/**
+	 * Step.
+	 *
+	 * @param speed
+	 *            the speed
+	 * @param r
+	 *            the r
+	 * @return the field
+	 */
+	public abstract Field step(CoordVector speed, Robi r);
+
+	/**
+	 * Stepped on you.
+	 *
+	 * @param r
+	 *            the r
+	 */
+	public abstract void steppedOnYou(Robi r);
+
+	/**
+	 * Investigate collision.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	public abstract void investigateCollision() throws Exception;
+
+
+
 	/**
 	 * Inits the field.
 	 */
 	public abstract void initField();
-	
+
 	/**
 	 * Adds the putty.
 	 *
-	 * @param p the putty that we add to the patches List
+	 * @param p
+	 *            the putty that we add to the patches List
 	 */
-	public void addPutty(Putty p) {
+	public void addPutty(Putty p) {			//R
 		patches.add(p);
 	}
-	
+
 	/**
 	 * Adds the oil.
 	 *
-	 * @param o the oil that we add to the patches List
+	 * @param o
+	 *            the oil that we add to the patches List
 	 */
-	public void addOil(Oil o) {
+	public void addOil(Oil o) {				//R
 		patches.add(o);
 	}
-	
+
 	/**
-	 * Gets the neighbours.
+	 * Gets the neighbours of the field.
 	 *
-	 * @return the neighbours
+	 * @return the List of neighbours.
 	 */
-	public List<Field> getNeighbours() {
+	public List<Field> getNeighbours() {	//R
 		return neighbours;
 	}
 
 	/**
 	 * Sets the neighbours.
 	 *
-	 * @param fields the new neighbours
+	 * @param fields
+	 *            the new neighbours
 	 */
-	public void setNeighbours(List<Field> fields) {
+	public void setNeighbours(List<Field> fields) {		//R
 		neighbours = fields;
 	}
 
 	/**
 	 * Gets the neighbour.
 	 *
-	 * @param direction the direction
+	 * @param direction
+	 *            the direction
 	 * @return the neighbour
 	 */
-	public abstract Field getNeighbour(CoordVector direction);/* {
-		Skeleton.printLastCalledFunction(id,new String[]{"direction",Skeleton.getClassName(direction)});
-		return null;
-	}*/
+	public abstract Field getNeighbour(CoordVector direction);
 
 	/**
 	 * Sets the coord.
 	 *
-	 * @param c the new coord
+	 * @param c
+	 *            the new coord
 	 */
 	public void setCoord(CoordVector c) {
 		coord = c;
