ad7f3633af4d44ed5b9133b25dec1f5e1d1aa02f
Dávid Sebők <sebidewyd@gmail.com>
2015-05-09 23:24:42 +0200

    Arena

--- Phobos/src/main/Main.java
+++ Phobos/src/main/Main.java
@@ -6,12 +6,13 @@ import ui.GameEventListener;
 import ui.GameWindow;
 
 public class Main {
-
+	static Arena mainArena;
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					mainArena = new Arena("arena");
 					GameWindow window = new GameWindow();
 					GameEventListener GEL = new GameEventListener();
 					window.setVisible(true);
