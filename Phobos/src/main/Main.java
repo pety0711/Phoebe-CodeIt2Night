0e9ee39fb180ca8f24f52d869035ede86c247078
Dávid Sebők <sebidewyd@gmail.com>
2015-05-09 23:31:47 +0200

    map

--- Phobos/src/main/Main.java
+++ Phobos/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					mainArena = new Arena("@C:\\map.csv");
+					mainArena = new Arena("C:\\map.csv");
 					GameWindow window = new GameWindow();
 					GameEventListener GEL = new GameEventListener();
 					window.setVisible(true);
