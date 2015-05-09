9e77e285fe9956641800b1313a68d8c3ccb09142
Dávid Sebők <sebidewyd@gmail.com>
2015-05-10 00:06:08 +0200

    Sárgaaaa

--- Phobos/src/ui/GamePanel.java
+++ Phobos/src/ui/GamePanel.java
@@ -1,11 +1,15 @@
 package ui;
 
+import java.awt.Color;
 import java.util.Map;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import main.CoordVector;
 
@@ -22,7 +26,7 @@ public class GamePanel extends JPanel{
 		gameArena.setRowSelectionAllowed(false);
 		gameArena.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+				{"a", null, null, null, null, null, null, null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
@@ -47,6 +51,7 @@ public class GamePanel extends JPanel{
 				return columnEditables[column];
 			}
 		});
+		JTextField textBox=new JTextField();
 		gameArena.getColumnModel().getColumn(0).setResizable(false);
 		gameArena.getColumnModel().getColumn(0).setPreferredWidth(15);
 		gameArena.getColumnModel().getColumn(1).setPreferredWidth(15);
@@ -62,10 +67,24 @@ public class GamePanel extends JPanel{
 		gameArena.getColumnModel().getColumn(11).setPreferredWidth(15);
 		gameArena.getColumnModel().getColumn(12).setPreferredWidth(15);
 		gameArena.getColumnModel().getColumn(13).setPreferredWidth(15);
-		gameArena.getColumnModel().getColumn(14).setPreferredWidth(15);
+		gameArena.getColumnModel().getColumn(14).setPreferredWidth(15);/*
+		TableColumn tc = gameArena.getColumnModel().getColumn(5);
+		textBox.setBackground(Color.RED);
+		tc.setCellEditor(new DefaultCellEditor(textBox));
+		textBox.setBackground(Color.BLUE);*/
 		this.add(gameArena);
+		MyCellRenderer mcr = new MyCellRenderer();
+		for (int columnIndex = 0; columnIndex < gameArena.getColumnCount(); columnIndex ++) {
+		            gameArena.getColumnModel().getColumn(columnIndex).setCellRenderer(mcr);
+		        }
 	}
-	
+	public class MyCellRenderer extends javax.swing.table.DefaultTableCellRenderer {
+	    public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, java.lang.Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+	        java.awt.Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	        cellComponent.setBackground(java.awt.Color.YELLOW);
+	        return cellComponent;
+	    }
+	}
 	public void draw(CoordVector coord, String type){
 		
 	}
