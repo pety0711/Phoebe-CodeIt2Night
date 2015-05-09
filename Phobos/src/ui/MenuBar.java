913d53e98bb4430ef21df3db30926995827b4600
RcsAkos <recseakos@gmail.com>
2015-05-10 01:01:14 +0200

    Menubar Event listener

--- Phobos/src/ui/MenuBar.java
+++ Phobos/src/ui/MenuBar.java
@@ -3,25 +3,14 @@ package ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
 
 public class MenuBar extends JMenuBar{
 
-	private JMenu file;
-	private JMenuItem newGame;
-	private JMenuItem exit;
-
-	private JMenu help;
-	private JMenuItem controls;
-
-	// TODO: ActionListener-ek beállítása + gyors gombok
 
 	public MenuBar() {
 
@@ -51,14 +40,13 @@ public class MenuBar extends JMenuBar{
 		class newGameAction implements ActionListener{
 			public void	actionPerformed(ActionEvent e) {
 				//TODO
-				System.out.println("newGame");
+				System.out.println("newGame");				
 			}
 		}
 		newGame.addActionListener(new newGameAction());
 		
 		class exitAction implements ActionListener{
 			public void	actionPerformed(ActionEvent e) {
-				
 				//TODO
 				System.out.println("exit");
 			}
