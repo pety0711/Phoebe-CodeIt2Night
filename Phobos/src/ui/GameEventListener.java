dd4f90b12d96d3fd61f24a39de9bc0a6c4cd2f19
pety0711 <pety.science@gmail.com>
2015-05-11 00:09:55 +0200

    új játék init

--- Phobos/src/ui/GameEventListener.java
+++ Phobos/src/ui/GameEventListener.java
@@ -80,16 +80,17 @@ public class GameEventListener implements KeyListener {
 	public void start() {
 		seconds = 0;
 		minutes = 2;
-		if(!ok){
-			Initialize(Arena.restart());
-			ok=false;
+		if (!ok) {
+			mainArena = Arena.restart();
+			robots = mainArena.getGamers();
+			ok = false;
 		}
 		timer.start();
 	}
 
 	public void exit() {
 		timer.stop();
-		ok=false;
+		ok = false;
 		String messageString = "";
 		if (robots.get(0).getPoints() < robots.get(1).getPoints()) {
 			messageString += "Winner is:  " + robots.get(1).id;
